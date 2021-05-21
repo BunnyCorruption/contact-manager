73e7d0ae8c96eb632468f3d4d0de4a85f302b774
Luis Mata <luivader2000@gmail.com>
2021-05-21 13:59:28 -0400

    update.php basic error correction

--- html/update.php
+++ html/update.php
@@ -18,7 +18,24 @@
         echo "<code>$stdout</code>";
         echo "<br>";
         echo "<p><a href='index.html'>Back to index.html</a></p>";
+        echo "<p>If you see something like:</p>";
+        echo "<code><span style='color:red' error:</span> Your local changes to the following files would be overwritten by merge:
+        html/update.php</code>";
+        echo "<p>Then click this, it will discard changes on the remote server (then try to pull latest again)</p>";
+        echo <<<EOT
+        <form action="update.php" method="post">
+        <button name="gitreset" value="citrus" type="submit">Pull latest</button>
+        </form>
+        EOT;
     }
+    if (isset($_POST['gitreset']) && !strcmp($_POST['gitreset'], "citrus"))
+    {
+        $stdout = shell_exec('cd /var/www && /usr/bin/git reset --hard HEAD^ 2>&1');
+        echo "<code>$stdout</code>";
+        echo "<br>";
+        echo "<p>Now, click pull latest again. or <a href='index.html'>Back to index.html</a></p>";
+    }
+    
     ?>
 </body>
 </html>
