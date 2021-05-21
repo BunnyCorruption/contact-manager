012c02e867d41c703e76617dfab9e9bfbfd0b90f
Luis Mata <luivader2000@gmail.com>
2021-05-21 13:30:57 -0400

    update.php added

---
+++ html/update.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pull Latest</title>
+</head>
+<body>
+    <form action="update.php" method="post">
+        <button name="update" value="grapefruit" type="submit"></button>
+    </form>
+    <?php
+    if (isset($_POST['update']) && !strcmp($_POST['update'], "grapefruit"))
+    {
+        $stdout = shell_exec('/usr/bin/git pull origin main');
+        echo "<code>$stdout</code>";
+        echo "<br>";
+        echo "<p><a href='index.html'>Back to index.html</a></p>";
+    }
+    ?>
+</body>
+</html>
