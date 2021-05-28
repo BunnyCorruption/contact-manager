be47963a21c4b467ed50ce8262638ea3f22f4912
Luis Mata <luivader2000@gmail.com>
2021-05-27 20:15:17 -0400

    trying to resolve http 500 error when returning from contact already exists

--- html/api/Add.php
+++ html/api/Add.php
@@ -36,8 +36,8 @@
     $result = $stmt->get_result();
     if ($result->num_rows != 0)
     {
-      // $stmt->close();
-      // $conn->close();
+      $stmt->close();
+      $conn->close();
       returnWithError("Contact Already Exists");
     } 
 
@@ -72,6 +72,7 @@
   {
     header('Content-type: application/json');
     echo $obj;
+    die();
   }
 
   # Function to return an error
